d692f08909aefa8e052d2688fe9e86da8ee906d7
Vishnu V V <ng.pingme@gmail.com>
2018-12-17 01:31:27 +0530

    Add the necessary routes for create and destroy favourites

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
+  resources :microposts,          only: [:create, :destroy] do
+
+    resource :favorite, only: [:create, :destroy]
+  end
   resources :relationships,       only: [:create, :destroy]
 end
