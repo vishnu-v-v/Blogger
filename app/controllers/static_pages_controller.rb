c6be0f8652e3779ddd364b3770079a65ac65e68d
Vishnu V V <ng.pingme@gmail.com>
2018-12-17 01:31:58 +0530

    Fetch the favourite posts under current user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,7 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = Micropost.favorited_by(params[:favorited]) if params[:favorited].present?
     end
   end
 
