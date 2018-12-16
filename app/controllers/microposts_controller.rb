b585b38e2bcde8f20fdb5ba250039401c343e49f
Vishnu V V <ng.pingme@gmail.com>
2018-12-16 14:13:16 +0530

    Add the functionality of reposting

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,7 +5,12 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created!"
+      flash[:success] =
+        if @micropost.micropost_id.present?
+          "Micropost retweeted!"
+        else
+          "Micropost created!"
+        end
       redirect_to root_url
     else
       @feed_items = []
@@ -21,11 +26,12 @@ class MicropostsController < ApplicationController
 
   private
 
-    def micropost_params
-      params.require(:micropost).permit(:content, :picture)
-    end
-    def correct_user
-      @micropost = current_user.microposts.find_by(id: params[:id])
-      redirect_to root_url if @micropost.nil?
-    end
+  def micropost_params
+    params.require(:micropost).permit(:content, :picture, :micropost_id)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
+  end
 end
